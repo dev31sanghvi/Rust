81d06b92e2419bcc39dbd258da31750f870dd2db
Dev Sanghvi <devsanghvi06@gmail.com>
2025-06-11 21:02:42 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    // println!("Hello, world!");
+let x:i8=10;
+
+
+    print!("x:{}", x);
+
+for i:i32 in 0..1000  {
+ x=x+100;
+
+
+
+}
+
+if is_male{
+    print!("male ");
+}else {
+    print!("not male " );
+}
+
+if is_male && is_above_18{
+    print!("you are legal male ");
+}
