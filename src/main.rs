4aebca6b305ad45cd8c078b3273b40dc74385ebe
Dev Sanghvi <devsanghvi06@gmail.com>
2025-06-12 12:41:59 +0530

    more practice

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ let x:i8=10;
 
     print!("x:{}", x);
 
-for i:i32 in 0..1000  {
- x=x+100;
+// for i:i32 in 0..1000  {
+//  x=x+100;
 
 
 
-}
+// }
 
 if is_male{
     print!("male ");
@@ -21,3 +21,78 @@ if is_male{
 if is_male && is_above_18{
     print!("you are legal male ");
 }
+
+// strings
+
+let greetings : &str=  "Hello, world!";
+for i in 0..100{
+    // ax=ax+
+}
+
+
+let char1:Option<char> = greetings.chars().nth(0);
+
+main();
+
+
+fn main (){
+    let is_even:bool=true ;
+
+if is_even {
+    println!("The number is even.");
+} else {
+    println!("The number is odd.");
+}
+
+use std::string;
+
+fn main(){
+    let sentence :String = String::from("Hello, world!");
+    let fisrt_word:String=get_first_word(&sentence);
+    println!("The first word is: {}", fisrt_word);
+}
+//iterating over and array
+
+fn get_first_word(sentence: &String) -> String {
+    let mut first_word = String::new();
+    for c in sentence.chars() {
+        if c == ' ' {
+            break;
+        }
+        first_word.push(c);
+    }
+    first_word
+}
+
+
+// simply iterating over an string
+
+fn main(){
+    let s= "dev sanghvio";
+    for c in s.chars(){ // char method ia basically an iterator that iterated over all elements .
+        print!("{} ", c);
+    }
+}
+
+fn main() {
+    let x= define_x();
+        println!("{}, world", x);
+
+    }
+
+    fn define_x() -> &'static str {
+        let x = "hello";
+        x
+    }
+
+
+    fn main() {
+        let x= define_x();
+            println!("{}, world", x);
+
+        }
+
+        fn define_x() -> &'static str {
+            let x = "hello";
+            x
+        }
